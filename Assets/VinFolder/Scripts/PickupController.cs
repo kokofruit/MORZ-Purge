b66b79520999971a3d6d5d1af92a754c5b2640c1
Vin Lettich <hgl6@pct.edu>
2025-09-16 13:19:56 -0400

    Cleaned up UI

---
+++ Assets/VinFolder/Scripts/PickupController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/*
+ * pickup parent controller used for health, ammo, weapons, etc.
+ * Written by: Vin and Mark
+ */
+
+public class PickupController : MonoBehaviour
+{
+
+    //variables
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
