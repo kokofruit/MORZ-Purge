56fc450d6f3774eaf783c73547e3b82ec8f366cf
Domenic Cannella <dac36@pct.edu>
2025-09-16 13:46:07 -0400

    Doors....

---
+++ Assets/Scripts/Domenic Cannella/DoorController.cs
@@ -0,0 +1,20 @@
+//Main Contributor: Domenic Cannella
+//Secondary Contributor:
+//Reviewer:
+//Description: A state machine that controls the opening and closing of doors.
+//Dates: 9/16/2025 - 
+
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
