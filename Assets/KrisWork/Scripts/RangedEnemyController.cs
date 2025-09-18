9e02bc942ce1502619e1c953403495e2cd5e9b80
Kris Herbert <klh55@pct.edu>
2025-09-18 12:59:05 -0400

    RangedEnemyController script created

---
+++ Assets/KrisWork/Scripts/RangedEnemyController.cs
@@ -0,0 +1,19 @@
+// Kris Herbert
+// 9/18/25-
+using UnityEngine;
+
+public class RangedEnemyController : EnemyController
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Start()
+    {
+        base.Start();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
