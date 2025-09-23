7abe3b5f99d9eb41a1e59e6238605ee99d7a5736
Moth Harper <mlh69@pct.edu>
2025-09-23 17:01:47 -0400

    Continue sound system

--- Assets/Scripts/Moth/SoundManager.cs
+++ Assets/Scripts/Moth/SoundManager.cs
@@ -11,11 +11,11 @@ public class SoundManager : MonoBehaviour
     // Singleton instance
     public static SoundManager instance;
 
+    // The prefab to use for playing audios
     [SerializeField] private AudioSource _soundPlayerPrefab;
-    [SerializeField] private AudioMixer _mixer;
 
-    // List of clips currently playing
-    private List<AudioClip> _playingClips = new();
+    // The mixer group for sound effects
+    [SerializeField] private AudioMixerGroup _fxMixer;
 
     // Set the instance or destroy if it's a duplicate
     private void Awake()
@@ -31,14 +31,14 @@ public class SoundManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Plays an audioclip, optionally with a random pitch.
+    /// Plays a sound effect, optionally with a random pitch.
     /// </summary>
     /// <param name="clip">The audio to play.</param>
     /// <param name="parent">The transform to attach the audio to.</param>
     /// <param name="volume">The volume the audio will play at.</param>
     /// <param name="pitch">The value that the random pitch range will be centered to. Use 1 for default pitch.</param>
     /// <param name="pitchFluctuation">The maximum amount the pitch can differ (postively or negatively) from the base pitch</param>
-    public void PlayAudio(AudioClip clip, Transform parent, float volume = 1f, float pitch = 1f, float pitchFluctuation = 0f)
+    public void PlayFXAudio(AudioClip clip, Transform parent, float volume = 1f, float pitch = 1f, float pitchFluctuation = 0f)
     {
         // Create an object to play the audio
         AudioSource player = Instantiate(_soundPlayerPrefab, parent);
@@ -46,6 +46,8 @@ public class SoundManager : MonoBehaviour
         // Set the attributes of the player
         player.clip = clip;
         player.volume = volume;
+        player.outputAudioMixerGroup = _fxMixer;
+        // set a random pitch
         float randomPitch = pitch + Random.Range(-pitchFluctuation, pitchFluctuation);
         player.pitch = randomPitch;
 
@@ -55,16 +57,5 @@ public class SoundManager : MonoBehaviour
         // After the sound is done playing, destroy the player
         float clipLength = clip.length;
         Destroy(player.gameObject, clipLength);
-    }
-
-    public void PlayRepeating(AudioClip clip, Transform parent, float volume = 1f, float pitch = 1f, float pitchFluctuation = 0.2f)
-    {
-        if (_playingClips.Contains(clip))
-        {
-            return;
-        }
-
-        _playingClips.Add(clip);
-        
     }
 }
