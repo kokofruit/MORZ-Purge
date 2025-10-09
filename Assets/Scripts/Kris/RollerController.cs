6ac5bc2ffdcb9bfc87e180cfabab629b344c2797
Kris Herbert <klh55@pct.edu>
2025-10-09 13:06:09 -0400

    Roller Enemy Competed?
    I add question mark because I am pretty sure that the Roller Enemy meets the description for it's health, movement, attack behavior, and damage output.

--- Assets/Scripts/Kris/RollerController.cs
+++ Assets/Scripts/Kris/RollerController.cs
@@ -1,8 +1,14 @@
+// Contributor: Kris Herbert
+// Reviewer: 
+// Description: Controller script used for the Roller Enemy.
 using UnityEngine;
 
 public class RollerController : RangedEnemyController
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Calls for the start that is used within RangedEnemyController which calls on EnemyController
+
+    //NOTE: RangedEnemyController Script could be used for this enemy but to have things better organized and seperated this RollerController will be used.
     void Start()
     {
         base.Start();
