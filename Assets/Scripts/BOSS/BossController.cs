a39e7b93f9455567e7297860841e91d87b1f4b5f
Moth Harper <mlh69@pct.edu>
2025-11-11 13:48:39 -0500

    Do like one single attack

--- Assets/Scripts/BOSS/BossController.cs
+++ Assets/Scripts/BOSS/BossController.cs
@@ -9,9 +9,9 @@
  * SPLIT STAGE 1
  * eventually split stage one into its own script
  */
-
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BossController : MonoBehaviour, IDamageable
@@ -20,7 +20,13 @@ public class BossController : MonoBehaviour, IDamageable
     [SerializeField] private float _health;
 
     // ATTACK ARRAY
-    protected Action[] _actions;
+    protected List<Action> _actions = new();
+
+    // GLOB ATTACK
+    [Header("Glob Attack")]
+    [SerializeField] protected float _globForce;
+    [SerializeField] protected GameObject _globPrefab;
+    [SerializeField] protected AudioClip _globAudio;
 
     // SOUNDS
     [Header("SFX")]
@@ -28,10 +34,22 @@ public class BossController : MonoBehaviour, IDamageable
     [SerializeField] protected AudioClip _damageAudio;
     [SerializeField] protected AudioClip _deathAudio;
 
+    // COMPONENTS
+    private Transform _playerTransform;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Cache player transform
+        _playerTransform = FindAnyObjectByType<PlayerController>().transform;
+
+        // Add actions to list
+        //_actions.Add(ChargeAttack);
+        //_actions.Add(BodySlam);
+        _actions.Add(ShootGlob);
+
+        // TEMPORARY
+        BossAwake();
     }
 
     // Update is called once per frame
@@ -62,8 +80,14 @@ public class BossController : MonoBehaviour, IDamageable
          * Spawn bugs
          * tentical barrage
          */
+        while (true)
+        {
+            Action nextAttack = _actions[UnityEngine.Random.Range(0, _actions.Count)];
+            print(nextAttack);
+            nextAttack.Invoke();
 
-        yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(3);
+        }
     }
 
     protected void ChargeAttack()
@@ -87,10 +111,21 @@ public class BossController : MonoBehaviour, IDamageable
     protected void ShootGlob()
     {
         /*
-         * boss lunges at player
+         * boss shoots glob at player
          * deal set player damage
-         * when hit wall/player reset back
          */
+
+        // calculate direction towards player
+        Vector3 direction = _playerTransform.position - transform.position;
+
+        // instantiate projectile
+        EnemyProjectileParent projectile = Instantiate(_globPrefab, direction.normalized + transform.position, Quaternion.identity).GetComponent<EnemyProjectileParent>();
+
+        // apply force
+        projectile.AddForce(direction.normalized * _globForce);
+
+        // play sound
+        SoundManager.instance.PlayFXAudio(_globAudio, transform, pitchFluctuation: 0.2f);
     }
 
     public void TakeDamage(float damage) 
@@ -110,14 +145,16 @@ public class BossController : MonoBehaviour, IDamageable
     // In the event of Boss death
     public void Die()
     {
-        //GameObject bugsplosion = Instantiate(_bugDeathExplosion.gameObject, transform.GetChild(0).position, quaternion.identity);
-        
-        // PLay sound on death
+        // Play sound on death
         SoundManager.instance.PlayFXAudio(_deathAudio, transform, pitchFluctuation: 0.2f);
         //Destroy(bugsplosion, 0.5f);
         StopAllCoroutines();
         Destroy(gameObject);
     }
 
+    protected void PlayerDamage(int damage)
+    {
+        _playerTransform.GetComponent<PlayerController>().SubtractHealth(damage);
+    }
 
 }
